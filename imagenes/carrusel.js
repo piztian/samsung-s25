906eb7cd112318d88c3365fab0666f022a958977
piztian <40465315+piztian@users.noreply.github.com>
2025-05-25 19:03:56 -0600

    Create carrusel.js

---
+++ imagenes/carrusel.js
@@ -0,0 +1,133 @@
+<div id="b2269" class="block-wrapper block-12-image-carousel-6-cols-fix-2"><section class="landing-block js-animation zoomIn text-center g-pt-30 g-pb-30" style="animation-duration: 1000ms; animation-play-state: running; animation-name: none;">
+	<div class="landing-block-slider container g-px-35 g-px-0--md" data-slider-autoplay="1" data-slider-autoplay-speed="3000" data-slider-slides-show="6" data-slider-arrows="7" data-slider-dots="0" data-slider-animation="1">
+		<div class="js-carousel slick-initialized slick-slider" data-autoplay="true" data-pause-hover="true" data-infinite="true" data-slides-show="6" data-arrows-classes="u-arrow-v1 g-absolute-centered--y g-width-45 g-height-45 g-font-size-30 g-transition-0_2 g-transition--ease-in g-color-gray-light-v1 g-color-primary--hover" data-arrow-left-classes="fa fa-angle-left g-left-minus-35" data-arrow-right-classes="fa fa-angle-right g-right-minus-35" data-pagi-classes="u-carousel-indicators-v1 g-absolute-centered--x g-pt-10" data-responsive="[{
+					 &quot;breakpoint&quot;: 1200,
+					 &quot;settings&quot;: {
+					   &quot;slidesToShow&quot;: 5
+					 }
+				   }, {
+					 &quot;breakpoint&quot;: 992,
+					 &quot;settings&quot;: {
+					   &quot;slidesToShow&quot;: 4
+					 }
+				   }, {
+					 &quot;breakpoint&quot;: 768,
+					 &quot;settings&quot;: {
+					   &quot;slidesToShow&quot;: 3
+					 }
+				   }, {
+					 &quot;breakpoint&quot;: 576,
+					 &quot;settings&quot;: {
+					   &quot;slidesToShow&quot;: 2
+					 }
+				   }]" data-init-classes-exclude="[{
+					 &quot;selector&quot;: &quot;.landing-block-card-carousel-item&quot;,
+					 &quot;class&quot;: &quot;col-6 col-sm-4 col-lg-2&quot;
+				   }, {
+					 &quot;selector&quot;: &quot;.js-carousel&quot;,
+					 &quot;class&quot;: &quot;row&quot;
+				   }]" data-speed="3000" data-slick="{&quot;animation&quot;:&quot;ease&quot;,&quot;arrows&quot;:true,&quot;dots&quot;:false}"><div class="js-prev u-arrow-v1 g-absolute-centered--y g-width-45 g-height-45 g-font-size-30 g-transition-0_2 g-transition--ease-in g-color-gray-light-v1 g-color-primary--hover fa fa-angle-left g-left-minus-35 slick-arrow" style=""></div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		<div class="slick-list draggable"><div class="slick-track" style="opacity: 1; width: 4416px; transform: translate3d(-1536px, 0px, 0px);"><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="-5" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img5.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="-4" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img6.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="-3" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img7.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="-2" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img8.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="-1" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img9.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide" style="width: 192px; height: auto;" data-slick-index="0" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img1.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide" style="width: 192px; height: auto;" data-slick-index="1" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img2.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide" style="width: 192px; height: auto;" data-slick-index="2" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img3.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-current slick-active" style="width: 192px; height: auto;" data-slick-index="3" aria-hidden="false">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img4.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-active" style="width: 192px; height: auto;" data-slick-index="4" aria-hidden="false">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img5.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-active" style="width: 192px; height: auto;" data-slick-index="5" aria-hidden="false">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img6.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-active" style="width: 192px; height: auto;" data-slick-index="6" aria-hidden="false">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img7.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-active" style="width: 192px; height: auto;" data-slick-index="7" aria-hidden="false">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img8.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide" style="width: 192px; height: auto;" data-slick-index="8" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img9.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="9" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img1.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="10" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img2.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="11" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img3.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="12" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img4.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="13" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img5.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="14" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img6.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="15" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img7.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="16" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img8.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div><div class="landing-block-card-carousel-item js-slide g-transition-0_2 g-px-10 slick-slide slick-cloned" style="width: 192px; height: auto;" data-slick-index="17" id="" aria-hidden="true">
+				<div class="landing-block-card-container d-flex justify-content-center align-items-center flex-column h-100">
+					<img class="landing-block-node-carousel-img img-fluid g-max-width-170--md g-brd-around g-brd-gray-light-v1--hover" src="https://cdn.bitrix24.site/bitrix/images/landing/business/200x150/img9.png" alt="" data-lazy-img="Y" loading="lazy">
+				</div>
+			</div></div></div><div class="js-next u-arrow-v1 g-absolute-centered--y g-width-45 g-height-45 g-font-size-30 g-transition-0_2 g-transition--ease-in g-color-gray-light-v1 g-color-primary--hover fa fa-angle-right g-right-minus-35 slick-arrow" style=""></div></div>
+	</div>
+</section></div>
